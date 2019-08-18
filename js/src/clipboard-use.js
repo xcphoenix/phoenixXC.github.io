35f398f5e7f35d5ba0618dafa67540003e37d673
PhoenixXC <bmingyy@qq.com>
2019-08-18 22:11:38 +0800

    Site updated: 2019-08-18 22:11:37

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 18 2019 16:54:06 GMT+0800 (中国标准时间)
+// build time:Sun Aug 18 2019 22:11:35 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
