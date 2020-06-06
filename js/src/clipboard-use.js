1e8f20b6dc13cc229c624ac7e029f3d8a4c8b74a
PhoenixXC <bmingyy@qq.com>
2020-06-06 10:01:34 +0800

    Site updated: 2020-06-06 10:01:34

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 06 2020 09:54:45 GMT+0800 (中国标准时间)
+// build time:Sat Jun 06 2020 10:01:31 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
